ba3bcff7a0dca85ed51a7d899bebc9886139a5e1
monarchbarry <monarchbarry>
2023-08-14 12:32:46 -0700

    Tasks completed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 97; i < 123; i++)
+    {
+        if (i != 101 && i != 113)
+        {
+            putchar(i);
+        }
+    }
+    putchar(n);
+    return (0);
+}
+
